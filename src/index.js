aceefa424f1866ba854ea5beba81d57d2986fc23
Gabriel Peralta <prates_peralta@hotmail.com>
2022-02-16 18:06:25 -0300

    adiciona demais rotas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -11,53 +11,104 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.header;
-  if(!username) {
-    return response.status(404).json({ error: 'Mensagem do erro'})
+  const { username } = request.headers;
+  const user = users.find((u) => u.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
   }
-  request.username = username;
+
+  request.user = user;
 
   return next();
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
   const { name, username } = request.body;
-  const newUser = { 
+  const newUser = {
     id: uuidv4(),
     name,
     username,
-    todos: []
-  }
-  const userAlreadyExists = users.some((user) => user.username === username)
+    todos: [],
+  };
+  const userAlreadyExists = users.find((user) => user.username === username);
 
-  if(userAlreadyExists) {
-    return response.status(400).json({ error: 'Mensagem do erro'});
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "Username already exists" });
   }
 
   users.push(newUser);
-  return response.status(200).json(newUser);
+  return response.status(201).json(newUser);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const { username } = request.header;
-  const findUser = users.find((user) => user.username === username);
-  return response.status(200).json(findUser.todos);
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  return response.json(user.todos);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+// É preciso alterar apenas o title e o deadline da tarefa que possua o id igual ao id presente nos parâmetros da rota.
+
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const findTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!findTodo) {
+    return response
+      .status(404)
+      .json({ error: "Cannot update an nonexistent todo" });
+  }
+
+  findTodo.title = title;
+  findTodo.deadline = new Date(deadline);
+  return response.json(findTodo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+  const findTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!findTodo) {
+    return response
+      .status(404)
+      .json({ error: "Cannot update an nonexistent todo" });
+  }
+
+  findTodo.done = true;
+
+  return response.json(findTodo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+  const findTodoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (findTodoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  user.todos.splice(findTodoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
