acebc0ab62d73c40f217f59e2ffe11664fc6ff23
Gabriel Peralta <prates_peralta@hotmail.com>
2022-02-16 16:01:20 -0300

    cria novo usuario

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header;
+  if(!username) {
+    return response.status(404).json({ error: 'Mensagem do erro'})
+  }
+  request.username = username;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const newUser = { 
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+  const userAlreadyExists = users.some((user) => user.username === username)
+
+  if(userAlreadyExists) {
+    return response.status(400).json({ error: 'Mensagem do erro'});
+  }
+
+  users.push(newUser);
+  return response.status(200).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.header;
+  const findUser = users.find((user) => user.username === username);
+  return response.status(200).json(findUser.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
